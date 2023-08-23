2518d1caf215cd720ab1153ce9c3842c105c1c21
Abhiraj <abhirajgtech@gmail.com>
2023-08-23 19:10:25 +0530

    Final Website

---
+++ assets/js/dynamic.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+
+    // Page First start
+    var Page_first_title = '100% Visibility into the entire security landscape';
+    var Page_first_description ='Recognize the paramount significance of 100% visibility into the entire security landscape. LemONE AI platform offers meticulous asset management, meticulous risk assessment, and unparalleled  transparency across on-prem, multi-cloud, and edge locations. This empowers CISOs, IT, Security Teams, and Application  Development Teams with actionable insights, ensuring informed decisions that enhance your organization s security posture';
+    // Page First end
+
+    // Page Second start
+    var Page_Second_title = 'Continuously Verify Your Security Posture in Real-Time';
+    var Page_Second_description= "In the realm of cybersecurity, the essence lies in staying steps ahead of potential threats. Harnessing the power of Machine Learning (ML), Artificial Intelligence (AI), and the capabilities of Large Language Models (LLM), the LemONE AI platform excels in anomaly detection and behavior analysis. By ingesting real-time data and leveraging threat intelligence, we empower you to preempt attacks, ensuring your digital environment remains fortified against evolving threats.";
+    // Page Second end
+
+    // Page Third start
+    var Page_third_title = "Automated Remediation";
+    var Page_third_description ="Elevate your cybersecurity strategy with our Cloud-integrated Automated Remediation Framework. Instantly detect threats, deploy AI-driven automated responses, and gain actionable insights. Stay protected in the ever-evolving digital landscape. Join us in securing your digital assets with cutting-edge expertise and technology.";
+    // Page Third end
+
+    // Page Four start
+    var Page_four_title = "Readymade Provisioning in the Cloud";
+    var Page_four_description = "With meticulously crafted secure deployment templates and playbooks, the LemONE AI platform empowers you to shift left in your security approach, drastically reducing costs while enhancing agility. Seamlessly respond to security incidents with confidence, knowing that our platform equips you with the tools needed to stay steps ahead in the ever-evolving threat landscape. Your cyber readiness journey starts here.";
+    // Page four end
+
+    // Page five Start
+    var Page_five_title = "Our Platform";
+    var Page_five_description = "Elevate cybersecurity with our visionary platform. Unmatched AI expertise ensures proactive defense, 100% visibility, and readymade cloud provisioning. Fortify your digital future today."
+    // Page five end
+
+    // Page six start
+    var Page_six_title = "About Us";
+    var Page_six_description = "LemONE.AI is led by  seasoned experts, and leaders in Cyber Security, AI, Cloud, and Networking, our founders embody innovation, experience, and a proven track record.";
+    // Page six end
+
+    // Email start 
+    var Email = "info@lemone.ai";
+    // Email end
+
+    $('.Page_first_title').html(Page_first_title);
+    $('.Page_first_description').html(Page_first_description);
+
+    $('.Page_Second_title').html(Page_Second_title);
+    $('.Page_Second_description').html(Page_Second_description);
+
+    $('.Page_third_title').html(Page_third_title);
+    $('.Page_third_description').html(Page_third_description);
+
+    $('.Page_four_title').html(Page_four_title);
+    $('.Page_four_description').html(Page_four_description);
+
+    $('.Page_five_title').html(Page_five_title);
+    $('.Page_five_description').html(Page_five_description);
+
+    $('.Page_six_title').html(Page_six_title);
+    $('.Page_six_description').html(Page_six_description);
+    
+    $('.Email').html(Email)
+    
+})
